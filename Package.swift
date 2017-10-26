0ce233fcbd994fc8795e220ddc2c14f7ed48e95a
Shane Vitarana <shanev@gmail.com>
2017-10-25 23:52:45 -0400

    Added HTTP dependency

--- Package.swift
+++ Package.swift
@@ -4,25 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Koneko",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Koneko",
-            targets: ["Koneko"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Koneko",
-            dependencies: []),
-        .testTarget(
-            name: "KonekoTests",
-            dependencies: ["Koneko"]),
-    ]
+  name: "Koneko",
+  products: [
+    .library(
+      name: "Koneko",
+      targets: ["Koneko"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/swift-server/http", from: "0.1.0"),
+  ],
+  targets: [
+    .target(
+      name: "Koneko",
+      dependencies: ["HTTP"]),
+    .testTarget(
+      name: "KonekoTests",
+      dependencies: ["Koneko", "HTTP"]),
+  ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Koneko
 
-A description of this package.
+Koneko is web µ-framework for Apple's [Swift HTTP server](https://github.com/swift-server/http).
+
+*Koneko is in the early stages of development and NOT currently production ready.*
