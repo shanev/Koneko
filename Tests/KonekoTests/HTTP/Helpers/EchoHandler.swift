3b8f4d02b994c5b985c4bece1b66738011272c0c
Shane Vitarana <shanev@gmail.com>
2017-10-29 19:26:26 -0400

    Downloaded swift server HTTP helpers

---
+++ Tests/KonekoTests/HTTP/Helpers/EchoHandler.swift
@@ -0,0 +1,31 @@
+// This source file is part of the Swift.org Server APIs open source project
+//
+// Copyright (c) 2017 Swift Server API project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+//
+
+import Foundation
+import HTTP
+
+/// Simple `HTTPRequestHandler` that just echoes back whatever input it gets
+class EchoHandler: HTTPRequestHandling {
+    func handle(request: HTTPRequest, response: HTTPResponseWriter ) -> HTTPBodyProcessing {
+        //Assume the router gave us the right request - at least for now
+        response.writeHeader(status: .ok, headers: ["Transfer-Encoding": "chunked", "X-foo": "bar"])
+        return .processBody { (chunk, stop) in
+            switch chunk {
+            case .chunk(let data, let finishedProcessing):
+                response.writeBody(data) { _ in
+                    finishedProcessing()
+                }
+            case .end:
+                response.done()
+            default:
+                stop = true /* don't call us anymore */
+                response.abort()
+            }
+        }
+    }
+}
