f1ecec8bbb69cb5f041b7ebdb388815f2700db92
Shane Vitarana <shanev@gmail.com>
2017-10-29 20:05:38 -0400

    Test for 404

--- Tests/KonekoTests/KonekoTests.swift
+++ Tests/KonekoTests/KonekoTests.swift
@@ -3,10 +3,6 @@ import XCTest
 @testable import Koneko
 
 class KonekoTests: XCTestCase {
-  func testExample() {
-    XCTAssertEqual("test", "test")
-  }
-
   func testGETRoot() {
     let receivedExpectation = self.expectation(description: "Received web response \(#function)")
 
@@ -43,8 +39,44 @@ class KonekoTests: XCTestCase {
     }
   }
 
+  func testGET404() {
+    let receivedExpectation = self.expectation(description: "Received web response \(#function)")
+
+    let router = Router()
+    router.get("/") { ctx in
+      ctx.response.writeHeader(status: .ok)
+      ctx.response.writeBody("GET /") 
+      ctx.response.done() 
+    }
+
+    let server = HTTPServer()
+    do {
+      try server.start(port: 0, handler: router.handler)
+      let session = URLSession(configuration: .default)
+      let url = URL(string: "http://localhost:\(server.port)/test404")!
+      print("Test \(#function) on port \(server.port)")
+      let dataTask = session.dataTask(with: url) { (responseBody, rawResponse, error) in
+        let response = rawResponse as? HTTPURLResponse
+        XCTAssertNil(error, "\(error!.localizedDescription)")
+        XCTAssertNotNil(response)
+        XCTAssertNotNil(responseBody)
+        XCTAssertEqual(Int(HTTPResponseStatus.notFound.code), response?.statusCode ?? 0)
+        receivedExpectation.fulfill()
+      }
+      dataTask.resume()
+      self.waitForExpectations(timeout: 5) { (error) in
+        if let error = error {
+          XCTFail("\(error)")
+        }
+      }
+      server.stop()
+    } catch {
+      XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
+    }
+  }
+
   static var allTests = [
-    ("testExample", testExample),
     ("testGETRoot", testGETRoot),
+    ("testGET404", testGET404),
   ]
 }
